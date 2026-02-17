fc173b5681edb2a02731ca1d405ba0f714456dd5
dhanalakshmin-n <dhanalakshmi.n@webknot.in>
2026-02-17 14:07:54 +0530

    req & res body using pydantic basemodel

--- app.py
+++ app.py
@@ -12,16 +12,30 @@ def read_root():    ##auto converts dict into json
 def greet_user(name: str):
     return{"message":f"hello {name}"}
 
-class User(BaseModel):   ##pydantic model
-    name:str
+# class User(BaseModel):   ##pydantic model
+#     name:str
+#     age:int
+
+# @app.post("/user")
+# def create_user(user:User):
+#     return{
+#         "message": f"user {user.name} created.",
+#         "age":user.age
+#     }
+
+
+ ##pydantic schema
+class UserCreate(BaseModel):
+    name: str
+    age:int
+    password:str
+
+class UserResponse(BaseModel):
+    name: str
     age:int
 
-@app.post("/user")
-def create_user(user:User):
-    return{
-        "message": f"user {user.name} created.",
-        "age":user.age
-    }
-
+@app.post("/user",response_model=UserResponse)
+def create_user(user:UserCreate):
+    return user
 
    
